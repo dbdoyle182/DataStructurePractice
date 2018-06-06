9d94a3f7477c6bb22cdc8c1f9d260bbeed982b7a
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-06 17:21:21 -0400

    add and remove complete on qs

--- QueueStackCombo/JSQueueStackCombo/QueueStack.js
+++ QueueStackCombo/JSQueueStackCombo/QueueStack.js
@@ -6,9 +6,21 @@ class Queue {
         this.second = new Stack();
     }
 
+    add(data) {
+        this.first.push(data);
+    }
+
     remove() {
         while(this.first.peek()) {
             this.second.push(this.first.pop());
         }
+
+        const data = this.second.pop();
+
+        while(this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+
+        return data;
     }
 }
