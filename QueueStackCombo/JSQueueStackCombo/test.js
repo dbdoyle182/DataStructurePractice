e8746a44bf35bd7c94de87a6cbaf2f2ba0ca0638
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-06 17:32:48 -0400

    second test added

--- QueueStackCombo/JSQueueStackCombo/test.js
+++ QueueStackCombo/JSQueueStackCombo/test.js
@@ -2,4 +2,11 @@ const Queue = require('./QueueStack')
 
 test('Queue is a class', () => {
     expect(typeof Queue.prototype.constructor).toEqual('function');
-})
+});
+
+test('can add elements to a queue', () => {
+    const q = new Queue();
+    expect(() => {
+      q.add(1);
+    }).not.toThrow();
+  });
