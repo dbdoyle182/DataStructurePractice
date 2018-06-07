c6d91e48c8f0d7e5e82ab7962355c9309453d688
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-07 14:47:48 -0400

    passes second test

--- LinkedList/test.js
+++ LinkedList/test.js
@@ -4,3 +4,7 @@ const Node = require('./node');
 test('List is a class', () => {
     expect(typeof List.prototype.constructor).toEqual('function')
 });
+
+test("Node is a class", () => {
+    expect(typeof Node.prototype.constructor).toEqual('function')
+});
