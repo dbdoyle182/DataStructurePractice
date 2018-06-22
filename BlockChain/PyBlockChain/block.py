20f302481ef8cb932b7264116e14ea115210b07b
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-21 20:07:44 -0400

    added pyblock to the folder

--- BlockChain/PyBlockChain/block.py
+++ BlockChain/PyBlockChain/block.py
@@ -17,5 +17,25 @@ class Block:
                     str(self.previous_hash)).encode())
         return sha.hexdigest()
 
-    def create_genesis_block():
+def create_genesis_block():
         return Block(0, date.datetime.now(), 'Genesis Block', "0")
+
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Hey! I'm block " + str(this_index)
+    this_hash = last_block.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
+
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+num_of_blocks_to_add = 20
+
+for i in range(0, num_of_blocks_to_add):
+    block_to_add = next_block(previous_block)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+
+    print(f"Block {block_to_add.index} has been added to the blockchain!")
+    print(f"Hash: {block_to_add.hash} \n")
